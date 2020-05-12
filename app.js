0d4164ef811d225c2be6dbdc831b4e35730b8c1b
Jamiena <z3febwpi@gmail.com>
2020-05-13 03:47:50 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-'use strict';
+'use strict'
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
+// 0番目と1番目はこの値で固定されているため、先に宣言しておく
+
 function fib(n) {
-    if (n === 0) {
-        return 0;
+    if (memo.has(n)) {
+        return memo.get(n);
+        // memo配列の中にある数値はそのままreturn
     }
-    else if (n === 1) {
-        return 1;
-    }
-    return fib(n - 1) + fib(n - 2);
+
+    const value = fib(n-1) + fib(n-2);
+    memo.set(n,value);
+    return value;
 }
 
 const length = 40;
-for (let i = 0; i <= length; i++) {
+for (let i=0; i <= length; i++) {
     console.log(fib(i));
 }
